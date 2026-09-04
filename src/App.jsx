cadd0dde652ba1b786c94c404f4f527096cbea59
FatimaEjaz294 <deathnote999@gmail.com>
2026-09-04 09:19:30 -0700

    fix build errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './css/App.css';
 import Favourite from './pages/Favourite';
-import Home from './pages/home'; 
+import Home from './pages/Home'; 
 import {Routes, Route} from "react-router-dom"
 import { MovieProvider } from './contexts/MovieContext';
 import NavBar from './components/NavBar';
